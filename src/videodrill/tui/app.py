ae4c767fc990710dc48184d0ac4d954fa6149535
Andy Sit <andysit173@gmail.com>
2024-08-26 03:04:02 -0400

    added LossessCut onevent

--- src/videodrill/tui/app.py
+++ src/videodrill/tui/app.py
@@ -4,19 +4,23 @@ from textual.binding import Binding
 from textual.widgets import DataTable, Footer, Static
 from textual.events import Paste
 from textual import work
-
 from typing import Literal
 
+from textual import on
 from videodrill.tui.lib.utils import pick_parser
+
 #custom
 from videodrill.tui.screens.modals import ModalAboutDialog, ModalInputDialog, ModalJumpDialog
 from videodrill.tui.screens.about import about_text
+
+from videodrill.tui.lib.utils import open_video
 
 class MyApp(App):
     BINDINGS = [
         Binding("i", "show_input_dialog", "Input Dialog"),
         Binding("a", "show_about_dialog", "About Dialog"),
         Binding("j", "show_jump_dialog", "Jump Dialog"),
+        Binding("d", "delete_row", "Delete Row")
     ]
 
     def __init__( self, in_filename: str, type : Literal['dict', 'tuple', 'list']) -> None:
@@ -38,18 +42,29 @@ class MyApp(App):
     def action_show_jump_dialog(self) -> None:
         self.push_screen(ModalJumpDialog("Jump to:"))
 
-    def on_mount(self) -> None:
+    def action_delete_row(self) -> None:
+        table : DataTable = self.query(DataTable)
+        row_key, _ = table.coordinate_to_cell_key(table.cursor_coordinate)
+        table.remove_row(row_key)
 
+    @on(DataTable.RowSelected)
+    def row_highlighted(self, message) -> None:
+        table : DataTable = self.query_one(DataTable)
+        row_key = message.row_key
+        self.log("Highlighted Row Key {}".format(row_key))
+        self.log(table.get_row(row_key=row_key))
+        open_video(table.get_row(row_key=row_key)[0])
+
+    def on_mount(self) -> None:
         table = self.query_one(DataTable)
+        table.show_cursor = True
         table.cursor_type = "row"
         table.zebra_stripes = True
         self.load_txt_file()
 
-
     def on_paste(self, event : Paste):
         print("CONSOLE DEBUG", event.text)
         # self.query_one("#content").update(JSON(files))
-
 
     def get_parser(self, type: str):
         return pick_parser(type=type)
@@ -91,6 +106,9 @@ class MyApp(App):
             table.sort("points")
         else:
             print("Column 'points' does not exist in the table.")
+
+
+
 
     # def action_todoactions
 if __name__ == "__main__":
--- src/videodrill/tui/lib/utils.py
+++ src/videodrill/tui/lib/utils.py
@@ -3,6 +3,18 @@
 """
 
 from ast import literal_eval
+import subprocess
+
+
+def open_video(in_filename : str):
+  try:
+    subprocess.run(['LosslessCut', in_filename], check=True)
+  except subprocess.CalledProcessError as e:
+    print(f"An error occurred: {e}")
+  except FileNotFoundError:
+    print("Media player executable not found.")
+  except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 def parse_dict(txt : str) -> dict:
   try:
@@ -40,3 +52,7 @@ def pick_parser(type : str) -> callable:
       return parse_tuple
 
     raise TypeError("Type not found {}".format(type))
+
+if __name__ == "__main__":
+  in_file = r"E:\Projects\2024\Video-Content-Pipeline\output-video\private\cery\clips\video00.mp4"
+  open_video(in_file)
